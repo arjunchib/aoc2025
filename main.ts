6baeb32f14579a58d56e8de79082eb6013c90636
Arjun Chib <arjunpatrickchib@gmail.com>
2025-12-01 01:31:13 -0500

    Add day 1

--- main.ts
+++ main.ts
@@ -2,14 +2,14 @@ const day = Bun.argv[2];
 
 try {
   const { part1, part2, test } = await import(`./solutions/day${day}`);
-  const input = Bun.file(`./inputs/day${day}.txt`);
+  const input = await Bun.file(`./inputs/day${day}.txt`).text();
 
   console.log(`PART I`);
-  console.log(`Test: ${part1(test)}`);
-  console.log(`Real: ${part1(input)}`);
+  console.log(`Test: ${part1(test.trim())}`);
+  console.log(`Real: ${part1(input.trim())}`);
   console.log(`\nPART II`);
-  console.log(`Test: ${part2(test)}`);
-  console.log(`Real: ${part2(input)}`);
+  console.log(`Test: ${part2(test.trim())}`);
+  console.log(`Real: ${part2(input.trim())}`);
 } catch (e: any) {
   if (e?.message?.startsWith("Cannot find module")) {
     console.log(`Creating new solution for day ${day}`);
--- solutions/day1.ts
+++ solutions/day1.ts
@@ -1,11 +1,52 @@
 export const test = `
-
+L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82
 `;
 
 export function part1(input: string) {
-  return 10;
+  const rotations = getRotations(input);
+  let dial = 50;
+  let zeros = 0;
+  for (const { amount, dir } of rotations) {
+    const mag = dir === "L" ? -1 : 1;
+    dial = mod(dial + amount * mag, 100);
+    if (dial === 0) zeros++;
+  }
+  return zeros;
 }
 
 export function part2(input: string) {
-  return 20;
+  const rotations = getRotations(input);
+  let dial = 50;
+  let zeros = 0;
+  for (const { amount, dir } of rotations) {
+    const mag = dir === "L" ? -1 : 1;
+    const pos = dial + amount * mag;
+    if (dir === "R") zeros += Math.trunc(pos / 100);
+    if (dir === "L") zeros += Math.abs(Math.trunc((pos - 100) / 100));
+    if (dir === "L" && dial === 0) zeros--; // technically doesn't pass 0
+    dial = mod(pos, 100);
+  }
+  return zeros;
+}
+
+function getRotations(input: string) {
+  return input.split("\n").map((line) => {
+    const dir = line.at(0) as "L" | "R";
+    const amount = parseInt(line.slice(1));
+    return { dir, amount };
+  });
+}
+
+// https://stackoverflow.com/questions/4467539/javascript-modulo-gives-a-negative-result-for-negative-numbers
+function mod(n: number, m: number) {
+  return ((n % m) + m) % m;
 }
